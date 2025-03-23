f1bb3d886700698d7e8214742f01e0b479751b54
irealycode <irealycode@localhost.localdomain>
2025-03-23 02:25:35 +0000

    projects description

--- js/index.js
+++ js/index.js
@@ -16,3 +16,18 @@ function switchContact(on) {
     const contact = document.getElementById("contact")
     contact.style.transform = `translateX(${on*-100}%)`
 }
+
+var last_opened = -1
+
+function showOrHideProject(n) {
+    const desc = document.getElementById(`desc-${n}`)
+    // console.log(desc.style)
+    desc.style.maxHeight = desc.style.maxHeight === "" || desc.style.maxHeight === "0px"?'160px':'0px'
+
+    if (last_opened !== -1 && last_opened !== n) {
+        const desc = document.getElementById(`desc-${last_opened}`)
+       desc.style.maxHeight = '0px'
+    }
+
+    last_opened = n
+}
