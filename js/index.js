ffa0c38169ff98779f384eb432099bc16b7eedbd
irealycode <irealycode@localhost.localdomain>
2025-03-23 00:50:45 +0000

    design improvement & contact

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 function switchPage(n) {
     const home = document.getElementById("home")
     home.style.transform = `translateY(${-n*100}%)`
-    
+
     const work = document.getElementById("work")
     work.style.transform = `translateY(${-n*100}%)`
 
     const about = document.getElementById("about")
     about.style.transform = `translateY(${-n*100}%)`
+}
 
+function switchContact(on) {
+    const home = document.getElementById("home")
+    home.style.transform = `translateX(${on*-100}%)`
+
+    const contact = document.getElementById("contact")
+    contact.style.transform = `translateX(${on*-100}%)`
 }
